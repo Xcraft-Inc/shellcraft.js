181bedf1867c0b99f559023b596389eaa0b30324
Mathieu Schroeter <mathieu@schroetersa.ch>
2014-12-05 18:49:29 +0100

    Fixes tests accordingly to the last bug fixed about the bad aysnc shutdown.

--- test/cli.js
+++ test/cli.js
@@ -1,75 +1,68 @@
 'use strict';
 
-var path       = require ('path');
-var should     = require ('should'); /* jshint ignore:line */
-var shellcraft = require ('../');
+var path   = require ('path');
+var should = require ('should'); /* jshint ignore:line */
 
-var runShellCraft = function (callback) {
-  var ext = path.join (__dirname, 'lib/extensions.js');
-  shellcraft.registerExtension (ext, function () {
-    shellcraft.begin ({}, callback);
-  });
+var runShellCraft = function (args, callback) {
+  var fork = require ('child_process').fork;
+
+  var prog = fork (path.join (__dirname, './sc.js'), args, {silent: true});
+  prog
+    .on ('error', callback)
+    .on ('close', callback);
 };
-
-process.exit = function (code) {
-  throw new Error (code);
-};
-
-beforeEach (function (done) {
-  delete process.argv;
-  process.argv = ['node', 'test'];
-  done ();
-});
 
 describe ('command: test# <arg>', function () {
   it ('test0 should retrieve the argument', function (done) {
-    process.argv[2] = 'test0';
-    process.argv[3] = 'the argument';
+    var args = [
+      'test0',
+      'the argument'
+    ];
 
-    runShellCraft (function () {
+    runShellCraft (args, function () {
       done ();
     });
   });
 
   it ('test1 should fail because the argument is missing', function (done) {
-    process.argv[2] = 'test1';
+    var args = ['test1'];
 
-    try {
-      runShellCraft (function () {
-        done ();
-      });
-    } catch (ex) {
-      ex.message.should.be.equal ('1');
-    }
-    done ();
+    runShellCraft (args, function (code) {
+      code.should.be.equal (1);
+      done ();
+    });
   });
 });
 
 describe ('command: test# [arg]', function () {
   it ('test2 should not fail if the argument is missing', function (done) {
-    process.argv[2] = 'test2';
+    var args = ['test2'];
     /* no argument provided to test2... */
 
-    runShellCraft (function () {
+    runShellCraft (args, function () {
       done ();
     });
   });
 
   it ('test3 should fail if the number of arguments is greater than 1', function (done) {
-    process.argv[2] = 'test3';
-    process.argv[3] = 'arg1';
-    process.argv[4] = 'arg2'; /* this argument should be dropped */
+    var args = [
+      'test3',
+      'arg1',
+      'arg2' /* this argument should be dropped */
+    ];
 
-    runShellCraft (function () {
+    runShellCraft (args, function () {
       done ();
     });
   });
 
   it ('test4 should retrieve the argument', function (done) {
-    process.argv[2] = 'test4';
-    process.argv[3] = 'the argument';
+    var args = [
+      'test4',
+      'the argument'
+    ];
 
-    runShellCraft (function () {
+    runShellCraft (args, function () {
       done ();
     });
   });
