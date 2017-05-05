dee6fb6c603c73330db580c763b8a41f263ff41d
Mathieu Schroeter <schroeter@epsitec.ch>
2017-05-05 12:14:27 +0200

    Update rules files

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,8 @@ module.exports = {
   rules: {
     // Other rules
     'no-console': 'off',
+    'valid-jsdoc': ['error', {requireReturn: false}],
+    eqeqeq: 'error',
     // The following rules can be used in some cases. See the README for more
     // information. (These are marked with `0` instead of `"off"` so that a
     // script can distinguish them.)
